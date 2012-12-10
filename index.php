498aa631a07168dc77e137d2c6f0fb783e21b3a9
Aurélien Bertron <aure_bertron@hotmail.com>
2012-12-10 21:28:09 +0100

    Ajoute les fichiers de base

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	
+	<title> Initiation HTML et CSS</title>
+</head>
+<body>
+	<div id="bandeau"><h1>Agence immobilière</h1></div>
+	<div id="menu">
+		<ul>
+			<li><a href="consultprix.php">Consultation des biens par prix</a></li>
+		</ul>
+	</div>
+	<div id="contenu">
+			
+		<p>
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed egestas 
+			faucibus nunc. Morbi quis neque laoreet ante varius rutrum. Nam arcu  
+			ipsum, blandit placerat, pharetra ut, pulvinar in, nulla. Vivamus  condimentum
+			elit eu faucibus sollicitudin, nisi metus fermentum mi,m lacinia nisl sit amet 
+			ipsum. Nam volutpat vulputate  lacus. Integer orci dui, lacinia non, blandit.
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed egestas 
+			faucibus nunc. Morbi quis neque laoreet ante varius rutrum. Nam arcu  
+			ipsum, blandit placerat, pharetra ut, pulvinar in, nulla. Vivamus  condimentum
+			elit eu faucibus sollicitudin, nisi metus fermentum mi,m lacinia nisl sit amet 
+			ipsum. Nam volutpat vulputate  lacus. Integer orci dui, lacinia non, blandit.
+		</p>	
+		
+	</div>
+	<div id="pied_page">Par Aurélien Bertron, aurelienbertron[at]gmail[dot]com</div>
+</body>
+</html>
