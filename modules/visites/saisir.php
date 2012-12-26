f457572fa99e35a32c6757781a77ab4a7d911f3d
Aurélien Bertron <aure_bertron@hotmail.com>
2012-12-26 11:16:24 +0100

    Ajoute les controles du formulaire de visite

--- modules/visites/saisir.php
+++ modules/visites/saisir.php
@@ -3,13 +3,13 @@
 $bien = null;
 $nbVisite = null;
 $idValide = null;
+$errors = array();
+$values = array();
 
 // Controle du paramètre (id du bien)
 $bdd = new BDD();
 $bien = $bdd->select("select idbien from bien where idbien = '".$_GET["id"]."';");
 $bdd->close();
-
-// Controle du formulaire
 
 if($bien != false && isset($_GET["id"]) && !empty($_GET["id"])) {
 	
@@ -17,7 +17,7 @@ if($bien != false && isset($_GET["id"]) && !empty($_GET["id"])) {
 	
 	$bdd = new BDD();
 	
-	$bien = $bdd->select("select b.titrebien, b.detailbien, b.adrbien, b.prixbien, b.idtype, b.photobien, t.nomtype from bien b, typebien t where idbien = '".$_GET["id"]."' AND t.idtype = b.idtype;");
+	$bien = $bdd->select("select b.idbien, b.titrebien, b.detailbien, b.adrbien, b.prixbien, b.idtype, b.photobien, t.nomtype from bien b, typebien t where idbien = '".$_GET["id"]."' AND t.idtype = b.idtype;");
 	
 	$nbVisite = $bdd->select("select COUNT(*) as nbvisite from visiter v where v.idbien = '".$_GET["id"]."';");
 	if(!$nbVisite)
@@ -25,10 +25,45 @@ if($bien != false && isset($_GET["id"]) && !empty($_GET["id"])) {
 	else
 		$nbVisite = $nbVisite[0]["nbvisite"];
 	
+	
+	// Controle du formulaire
+	if(isset($_POST["filled"]) && $_POST["filled"] == "true") {
+	
+		if(!isset($_POST["nom"]) || empty($_POST["nom"]))
+			$errors[] = "Vous devez entrer un nom";
+		else
+			$values["nom"] = $_POST["nom"];
+	
+		if(!isset($_POST["adresse"]) || empty($_POST["adresse"]))
+			$errors[] = "Vous devez entrer une adresse";
+		else
+			$values["adresse"] = $_POST["adresse"];
+	
+		if(!isset($_POST["tel"]) || empty($_POST["tel"]))
+			$errors[] = "Vous devez entrer un numéro de téléphone correct";
+		else
+			$values["tel"] = $_POST["tel"];
+	
+		if(!isset($_POST["email"]) || empty($_POST["email"]))
+			$errors[] = "Vous devez entrer une adresse e-mail valide";
+		else
+			$values["email"] = $_POST["email"];
+	
+		if(!isset($_POST["dispo"]) || empty($_POST["dispo"]))
+			$errors[] = "Vous devez entrer des disponibilités valides";
+		else
+			$values["dispo"] = $_POST["dispo"];
+	}
+	
+	
+	
+	
+	
+	
 	$bdd->close();
 }
 else {
 	$idValide = false;
 }
 
-echo $twig->render("visites_saisir.html", array("idValide" => $idValide, "bien" => $bien, "nbVisite" => $nbVisite));
+echo $twig->render("visites_saisir.html", array("values" => $values, "idValide" => $idValide, "bien" => $bien, "nbVisite" => $nbVisite, "errors" => $errors));
