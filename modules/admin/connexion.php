3a8871d566e37cf4e5e489a65a3dc7760ed0cbd7
Darknautilus <aurelienbertron@gmail.com>
2013-03-03 18:40:45 +0100

    Ajoute l'espace de connexion pour admin

--- modules/admin/connexion.php
+++ modules/admin/connexion.php
@@ -4,10 +4,17 @@ $redirect = false;
 $errors = array();
 $values = array();
 
+if(isset($_GET["membtype"]))
+  $membtype = $_GET["membtype"];
+else
+  $membtype = null;
+
 // Si l'on a rempli le formulaire et qu'on est pas loggué
 if(isset($_POST["filled"]) && !isLogged()) {
   
   if(isset($_POST["client"])) {
+    $membtype = "client";
+    
   	// Controles de surface
   	if(empty($_POST["numclient"])) {
   		$errors[] = "Veuillez entrer un numéro client.";
@@ -31,17 +38,48 @@ if(isset($_POST["filled"]) && !isLogged()) {
   				$errors[] = "Cet identifiant client n'existe pas";
   		}
   		else {
-  			// On controle le mot de passe
+  			// On controle l'adresse email
   			if($member[0]["emailclient"] != $_POST["email"]) {
   				$errors[] = "L'adresse email est incorrecte";
   			}
   			// On rajoute un champ pour différencier le client de l'admin
   			$member[0]["client"] = true;
+  			$infos = $member[0];
   		}
   	}
   }
   else if(isset($_POST["admin"])) {
+    $membtype = "admin";
     
+    // Controles de surface
+    if(empty($_POST["nomcompte"])) {
+      $errors[] = "Veuillez entrer un nom de compte.";
+    }
+    else {
+      $values["nomcompte"] = $_POST["nomcompte"];
+      if(empty($_POST["password"]))
+        $errors[] = "Veuillez entrer un mot de passe.";
+      else
+        $values["password"] = $_POST["password"];
+    }
+    
+    // Controles en profondeur
+    if(empty($errors)) {
+      $infos = array();
+      if($values["nomcompte"] != "adm") {
+        $errors[] = "Nom de compte inconnu";
+      }
+      else {
+        if($values["password"] != "adm") {
+          $errors[] = "Mot de passe inconnu";
+        }
+        else {
+          $infos["nomcompte"] = $values["nomcompte"];
+          $infos["admin"] = true;
+        }
+      }
+      
+    }
     
   }
   
@@ -49,12 +87,12 @@ if(isset($_POST["filled"]) && !isLogged()) {
 	if(empty($errors)) {
 	  
 		$_SESSION["logged"] = true;
-		$_SESSION["infos"] = $member[0];
-		$GLOBALS["infos"] = $member[0];
+		$_SESSION["infos"] = $infos;
+		$GLOBALS["infos"] = $infos;
 		
 		if(isset($_POST["cookie"])) {
 		  creerCookie("logged", true);
-		  creerCookie("infos",$member[0]);
+		  creerCookie("infos",$infos);
 		}
 		
 		// On redirige
@@ -62,4 +100,4 @@ if(isset($_POST["filled"]) && !isLogged()) {
 	}
 }
 
-echo $twig->render("admin_connexion.html", array("values" => $values, "errors" =>  $errors));
+echo $twig->render("admin_connexion.html", array("values" => $values, "errors" =>  $errors, "membtype" => $membtype));
